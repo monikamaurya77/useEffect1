f5181b669f626cffdc9aee97c3ccb258183b8497
Monika Maurya <96534851+monikamaurya77@users.noreply.github.com>
2022-11-10 22:24:03 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+
+const Home = () => {
+
+    const [query, setQuery] = useState("Panner");
+
+    const [data, setData] = useState([]);
+
+    const [isClicked, setIsClicked] = useState(false);
+
+    useEffect(() => {
+        fetch(`https://api.edamam.com/api/recipes/v2?q=${query}&app_id=9b824484&app_key=a51461b1d0e10d02356c4c3044610afe&type=public`).then(
+            (Response) => Response.json()
+        ).then(
+            (data) => {
+                console.log(data.hits);
+                const arrayData = data.hits;
+                setData(arrayData);
+            }
+        )
+
+    }, [isClicked])
+
+    return (
+        <div className="container">
+         
+            <input onChange={(e)=>{setQuery(e.target.value)}} type="text" className="input" placeholder="search recipes"></input>
+            <button onClick={()=>{setIsClicked((prevState)=>!prevState)}} className="button">Search</button>
+            <h5>Showing result for  {query}</h5>
+            {
+                data.map((item, i) => {
+                    return <div key={i}>
+                        <h3>{item.recipe.label}</h3>
+                        <img className="img" src={item.recipe.image} />
+                    </div>
+                })
+
+            }
+
+           
+        </div>
+
+    );
+}
+
+export default Home;
